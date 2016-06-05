0ac6d43ab7bf1772a44a9f32afecfb4514d0e048
krwani <kalpeshwani1@gmail.com>
2016-06-05 16:53:08 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,23 @@
 function myFunction() {
 document.getElementById("search").value=null;
 }
-
+window.onload = function () {
+ var chart = new CanvasJS.Chart("graphs",
+ {
+  animationEnabled: true,
+  title:{
+   text: "Section Wise Performance"
+  },
+  data: [
+  {
+   type: "column", //change type to bar, line, area, pie, etc
+   dataPoints: [
+    { label: "Above Average", y: 33 },
+    { label: "Below Average", y: 66 },
+   ]
+  }
+  ]
+ });
+ chart.render();
+}
 
