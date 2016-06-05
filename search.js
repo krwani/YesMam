b06078e6d781d13d4f0518dbb0e39b20774c39b5
krwani <kalpeshwani1@gmail.com>
2016-06-05 15:53:25 +0530

    Create search.js

---
+++ search.js
@@ -0,0 +1,21 @@
+function myFunction() {
+document.getElementById("search").value=null;
+}
+
+var chrt = document.getElementById("graphs").getContext("2d");
+
+var data = {
+    labels: ["Above Average", "Below Average"], //x-axis
+    datasets: [
+        {
+            label: "Section Wise Performance", //optional
+            fillColor: "blue(220,220,220,0.8)",
+            
+            highlightFill: "blue(220,220,220,0.75)",
+            
+            data: [33, 66] // y-axis
+        }
+    ]
+};
+
+var Chart = new Chart(chrt).Bar(data);
